f8a4cedbd7a79417e5a8476c15f52822999ee4dc
Santiago Pintus <santiagopintus@gmail.com>
2021-10-20 18:24:47 -0300

    Successfully bring characters from API and show them in the console! :D

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,43 @@ document.addEventListener('DOMContentLoaded', () => {
     main();
 })
 
-characters = [];
+const characters = [];
+
+/* URLS */
+allCharactersUrl = "http://hp-api.herokuapp.com/api/characters";
+studentsUrl = "http://hp-api.herokuapp.com/api/characters/students";
+staffUrl = "http://hp-api.herokuapp.com/api/characters/staff";
+/* Houses URLS*/
+griffUrl = "https://hp-api.herokuapp.com/api/characters/house/gryffindor";
+slythUrl = "https://hp-api.herokuapp.com/api/characters/house/slytherin";
+huffUrl = "https://hp-api.herokuapp.com/api/characters/house/hufflepuff";
+ravenUrl = "https://hp-api.herokuapp.com/api/characters/house/ravenclaw";
 
 function main() {
     getCharacters();
 }
 
 function getCharacters() {
-    console.log("Getting characters");
+
+    let promise = fetch(allCharactersUrl)
+        .then(response => response.json())
+        .then((charactersResponse) => {
+            for (let character of charactersResponse) {
+                characters.push(character);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    
+    promise.then(showCharacters);
+}
+
+function showCharacters() {
+    const container = document.getElementById('charactersContainer');
+
+    for (let character of characters) {
+        console.log(character);
+        const characterElement = document.createElement('div');
+    }
 }
